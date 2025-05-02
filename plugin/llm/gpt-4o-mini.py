dd9602b8dd1ca3e09d74bf2476d1b5faa69245cf
stsr1284 <1599zzang@gmail.com>
2025-05-02 19:03:10 +0900

    fix: remove api

--- plugin/llm/gpt-4o-mini.py
+++ plugin/llm/gpt-4o-mini.py
@@ -3,7 +3,6 @@ from langchain_openai import ChatOpenAI
 llmModel = ChatOpenAI(
     model="gpt-4o-mini",
     temperature=0,
-    api_key="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",
 )
 
 
